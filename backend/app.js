6ea56808ec86396d9c9b78d0975c8d59fe9c9c28
sak003 <sakshikuthe336@gmail.com>
2025-12-23 18:15:30 +0530

    Fix PORT binding for Render deployment

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,8 @@ import  dbConnection from "./database/dbconnection.js"; //to connect database
 import { errorMiddleware } from "./error/error.js";
 import reservationRoute from "./route/reservationRoute.js"; //importing reservation route
 const app = express();
-dotenv.config({ path: './config/config.env' });
+//dotenv.config({ path: './config/config.env' });
+dotenv.config(); //configuring dotenv
 
 //code to connect backent to frontend
 app.use(cors({
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import app from './app.js';
 
-app.listen(process.env.port, () => {
-    console.log(`Server is running on port ${process.env.port}`);
+const PORT= process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
